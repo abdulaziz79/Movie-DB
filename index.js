26bd7361dde1a1a98f95825796e14c629698b456
abdulaziz <aboudecharkawi@gmail.com>
2023-09-22 17:00:28 +0300

    step4

--- index.js
+++ index.js
@@ -19,4 +19,36 @@ app.get("/time", (req, res) =>{
 
     res.status(200).send(hours+":"+second)
 })
-app.listen(3000)
+
+app.get("/hello/:id?", (req, res) =>{
+
+    let id = req.params.id;
+    let message;
+    if(id){
+        message="Hello "+ id
+    }else  {
+        message="Hello user "
+    }
+
+    res.json({status:200, message})
+})
+
+app.get("/search", (req, res) =>{
+
+    let name=req.query.s;
+    if(name)
+    
+
+    res.json({status:200, message :"ok", data:name})
+
+    else{
+        res.json({status:500,error:true, message :"you have to provide a search"})
+    }
+});
+
+
+
+
+app.listen(3000);
+
+
