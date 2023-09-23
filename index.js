ef7253769e44678cd63469638c7c543a9457720d
abdulaziz <aboudecharkawi@gmail.com>
2023-09-23 13:02:21 +0300

    step5

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app =express()
 
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }]
+
 app.get("/", (req, res) =>{
     console.log("here")
     res.send("ok")
@@ -46,6 +52,24 @@ app.get("/search", (req, res) =>{
     }
 });
 
+app.get("/movies/create", (req, res) =>{
+
+})
+
+app.get("/movies/read", (req, res) =>{
+
+    res.json({status:200, data:movies})
+
+    
+})
+
+app.get("/movies/update", (req, res) =>{
+    
+})
+
+app.get("/movies/delete", (req, res) =>{
+    
+})
 
 
 
